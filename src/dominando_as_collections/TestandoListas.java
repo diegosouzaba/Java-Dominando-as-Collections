df529cd1d1b9e1c1265484026703fbb17934054c
Diego souza silva <diego.souzaba@hotmail.com>
2019-02-27 14:53:39 -0300

    Add files via upload

--- src/dominando_as_collections/TestandoListas.java
+++ src/dominando_as_collections/TestandoListas.java
@@ -1,6 +1,7 @@
 package dominando_as_collections;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class TestandoListas {
 
@@ -10,14 +11,22 @@ public class TestandoListas {
 		String aula1 = "Conhecendo mais de listas";
 		String aula2 = "modelando a classe aula";
 		String aula3 = "trabalhando com cursos e sets";
+		String aula4 = "A ";
+		String aula5 = "B ";
+		String aula6 = "C ";
 		
 		ArrayList<String> aulas = new ArrayList<>();
 		aulas.add(aula1);
 		aulas.add(aula2);
 		aulas.add(aula3);
+		aulas.add(aula4);
+		aulas.add(aula5);
+		aulas.add(aula6);
+		
 		
 		System.out.println(aulas);
 		aulas.remove(0);
+		System.out.println("\n\n\n\n");
 		
 		for (String aula : aulas) {
 			System.out.println(aula);
@@ -25,14 +34,20 @@ public class TestandoListas {
 		
 		String primeiraAula = aulas.get(0);
 		System.out.println("A primeira aula e :"+primeiraAula);
+		System.out.println("\n\n\n\n");
 		
 		for(int i = 0;i < aulas.size();i++) {
 			
 			System.out.println("aula :"+ aulas.get(i));
 			
 		}
+		System.out.println("\n\n\n\n");
 		
 		aulas.forEach(aula ->{System.out.println("Percorrendo :"+ aula);});
+		System.out.println("\n\n\n\n");
+		
+		Collections.sort(aulas);
+		System.out.println(aulas);
 		
 
 	}
